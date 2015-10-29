4c18c32fff688f54d61cd9787d47ebca5d9b0aac
Taylor Fausak <taylor@fausak.me>
2015-10-29 12:46:48 -0500

    Add some helper functions to the map type class

--- src/Ax/Map.purs
+++ src/Ax/Map.purs
@@ -1,9 +1,14 @@
 module Ax.Map
   ( Map
   , map
+  , void
+  , (<$)
+  , (<$>)
+  , ($>)
   ) where
 
 import Ax.Compose ((>>))
+import Ax.Unit (Unit(), unit)
 
 foreign import jsMapArray :: forall a b. (a -> b) -> Array a -> Array b
 
@@ -15,3 +20,18 @@ instance mapArray :: Map Array where
 
 instance mapFunction :: Map (Function a) where
   map f g = g >> f
+
+void :: forall f a. (Map f) => f a -> f Unit
+void f = map (\ _ -> unit) f
+
+(<$) :: forall f a b. (Map f) => a -> f b -> f a
+(<$) x f = map (\ _ -> x) f
+infixl 4 <$
+
+(<$>) :: forall f a b. (Map f) => (a -> b) -> f a -> f b
+(<$>) f x = map f x
+infixl 4 <$>
+
+($>) :: forall f a b. (Map f) => f b -> a -> f a
+($>) f x = map (\ _ -> x) f
+infixl 4 $>
