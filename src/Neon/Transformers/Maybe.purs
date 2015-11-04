c76db1b0e79f9d1b4285729f96fbc527ab655bf4
Taylor Fausak <taylor@fausak.me>
2015-11-04 12:28:17 -0600

    Add more instances

--- src/Neon/Transformers/Maybe.purs
+++ src/Neon/Transformers/Maybe.purs
@@ -3,13 +3,25 @@ module Neon.Transformers.Maybe
   , runMaybeT
   ) where
 
+import Neon.Types.HasAlternative (HasAlternative)
 import Neon.Types.HasApply (HasApply, apply)
-import Neon.Types.HasBind (HasBind, bind)
+import Neon.Types.HasBind (HasBind, bind, (>>=))
+import Neon.Types.HasCompose ((>>))
+import Neon.Types.HasEmpty (HasEmpty, empty)
+import Neon.Types.HasLift (HasLift)
 import Neon.Types.HasMap (HasMap, map)
 import Neon.Types.HasPure (HasPure, pure)
 import Neon.Values.Maybe (Maybe(Just, Nothing))
 
 newtype MaybeT m a = MaybeT (m (Maybe a))
+
+-- TODO: Can this be written with only a "HasApply" constraint?
+instance maybeTHasAlternative :: (HasBind m) => HasAlternative (MaybeT m) where
+  alternative m1 m2 = MaybeT do
+    x <- runMaybeT m1
+    case x of
+      Just _ -> pure x
+      Nothing -> runMaybeT m2
 
 -- TODO: There has to be a better way to write this. Is it possible to write
 --   with only a "HasApply" constraint?
@@ -26,7 +38,12 @@ instance maybeTHasBind :: (HasBind m) => HasBind (MaybeT m) where
       Just z -> runMaybeT (f z)
       Nothing -> pure Nothing
 
--- TODO: HasLift
+-- TODO: If alternative instance doesn't use bind, this doesn't either.
+instance maybeTHasEmpty :: (HasBind m) => HasEmpty (MaybeT m) where
+  empty = MaybeT (pure empty)
+
+instance maybeTHasLift :: HasLift MaybeT where
+  lift x = MaybeT (x >>= pure >> pure)
 
 instance maybeTHasMap :: (HasMap m) => HasMap (MaybeT m) where
   map f (MaybeT x) = MaybeT (map (map f) x)
