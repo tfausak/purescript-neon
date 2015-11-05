c0b6a9dc0115302433314894c39d6e56b9f56a6f
Taylor Fausak <taylor@fausak.me>
2015-11-04 18:35:45 -0600

    Add the HasState type class

--- src/Neon/Types.purs
+++ src/Neon/Types.purs
@@ -20,6 +20,7 @@ module Neon.Types
   , module Neon.Types.HasOr
   , module Neon.Types.HasPure
   , module Neon.Types.HasShow
+  , module Neon.Types.HasState
   , module Neon.Types.HasSubtract
   , module Neon.Types.HasZero
   , module Neon.Types.IsBounded
@@ -45,6 +46,7 @@ import Neon.Types.HasOne
 import Neon.Types.HasOr
 import Neon.Types.HasPure
 import Neon.Types.HasShow
+import Neon.Types.HasState
 import Neon.Types.HasSubtract
 import Neon.Types.HasZero
 import Neon.Types.IsBounded
---
+++ src/Neon/Types/HasState.purs
@@ -0,0 +1,25 @@
+module Neon.Types.HasState
+  ( HasState
+  , state
+  , getState
+  , setState
+  ) where
+
+import Neon.Primitives.Function (constant, (|>))
+import Neon.Types.HasBind (HasBind)
+import Neon.Values.Pair (Pair(), pair)
+import Neon.Values.Unit (Unit(), unit)
+
+-- | Laws:
+-- | - `do { getState ; getState } = getState`
+-- | - `do { setState x ; setState y } = setState y`
+-- | - `do { setState x ; getState } = setState x $> x`
+-- | - `do { s <- getState ; setState s } = pure unit`
+class (HasBind m) <= HasState s m where
+  state :: forall a. (s -> (Pair a s)) -> m a
+
+getState :: forall m s. (HasState s m) => m s
+getState = state \ x -> pair x x
+
+setState :: forall m s. (HasState s m) => s -> m Unit
+setState x = x |> pair unit |> constant |> state
