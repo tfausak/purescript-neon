eaf358525adc5794a6258cc53087d45152c09473
Taylor Fausak <taylor@fausak.me>
2015-11-09 14:54:33 -0600

    Add tests for MaybeT

--- test/Neon/Transformers.purs
+++ test/Neon/Transformers.purs
@@ -1,7 +1,9 @@
 module Test.Neon.Transformers where
 
 import Neon
-import Test.Core (Test())
+import Neon.Transformers -- NOTE: purescript/purescript#1594
+import Neon.Values -- NOTE: purescript/purescript#1594
+import Test.Core (Test(), (==>))
 
 testTransformers :: Test
 testTransformers = do
@@ -10,7 +12,15 @@ testTransformers = do
 
 testMaybeT :: Test
 testMaybeT = do
-  warn "? MaybeT" -- TODO
+  let x = MaybeT (Identity (Just 1))
+  runMaybeT (MaybeT (Identity Nothing) <|> x) ==> Identity (Just 1)
+  runMaybeT (MaybeT (Identity (Just (+ 1))) <*> x) ==> Identity (Just 2)
+  runMaybeT (x >>= ((+ 1) >> pure)) ==> Identity (Just 2)
+  runMaybeT empty ==> Identity (Nothing :: Maybe Unit)
+  runMaybeT (lift (pure 1)) ==> Identity (Just 1)
+  runMaybeT ((+ 1) <$> x) ==> Identity (Just 2)
+  runMaybeT (pure 1) ==> Identity (Just 1)
+  runMaybeT x ==> Identity (Just 1)
 
 testStateT :: Test
 testStateT = do
