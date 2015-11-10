a15c99fa4fdce450649339ecfe3d702f96eddd0a
Taylor Fausak <taylor@fausak.me>
2015-11-10 13:19:40 -0600

    Add a bunch of foldable helper functions

--- src/Neon/Types/HasFold.purs
+++ src/Neon/Types/HasFold.purs
@@ -2,7 +2,22 @@ module Neon.Types.HasFold
   ( HasFold
   , foldl
   , foldr
+  , foldMap
+  , length
+  , isPresent
+  , isBlank
+  , contains
+  , sum
+  , product
   ) where
+
+import Neon.Primitives.Function (constant)
+import Neon.Types.HasAdd ((+))
+import Neon.Types.HasEqual (HasEqual, (==))
+import Neon.Types.HasMultiply ((*))
+import Neon.Types.HasNot (not)
+import Neon.Types.HasOne (HasOne, one)
+import Neon.Types.HasZero (HasZero, zero)
 
 foreign import nativeFoldlArray :: forall a b. (b -> a -> b) -> b -> Array a -> b
 foreign import nativeFoldrArray :: forall a b. (a -> b -> b) -> b -> Array a -> b
@@ -14,3 +29,24 @@ class HasFold f where
 instance arrayHasFold :: HasFold Array where
   foldl f y xs = nativeFoldlArray f y xs
   foldr f y xs = nativeFoldrArray f y xs
+
+foldMap :: forall f a m. (HasFold f, HasZero m) => (a -> m) -> f a -> m
+foldMap f xs = foldl (\ a e -> a + f e) zero xs
+
+length :: forall f a. (HasFold f) => f a -> Int
+length xs = foldMap (constant 1) xs
+
+isPresent :: forall f a. (HasFold f) => f a -> Boolean
+isPresent xs = foldMap (constant true) xs
+
+isBlank :: forall f a. (HasFold f) => f a -> Boolean
+isBlank xs = not (isPresent xs)
+
+contains :: forall f a. (HasFold f, HasEqual a) => a -> f a -> Boolean
+contains x xs = foldMap (== x) xs
+
+sum :: forall f a. (HasFold f, HasZero a) => f a -> a
+sum xs = foldl (+) zero xs
+
+product :: forall f a. (HasFold f, HasOne a) => f a -> a
+product xs = foldl (*) one xs
--- test/Neon/Types/HasFold.purs
+++ test/Neon/Types/HasFold.purs
@@ -8,3 +8,13 @@ testHasFold = do
   info "Neon.Types.HasFold"
   foldl (+) "a" ["b", "c"] ==> "abc"
   foldr (+) "a" ["b", "c"] ==> "bca"
+  foldMap (+ "!") ["a", "b"] ==> "a!b!"
+  length [1, 2] ==> 2
+  isPresent ([] :: Array Unit) ==> false
+  isPresent [unit] ==> true
+  isBlank ([] :: Array Unit) ==> true
+  isBlank [unit] ==> false
+  contains 1 [1, 2] ==> true
+  contains 0 [1, 2] ==> false
+  sum [1, 2] ==> 3
+  product [1, 2] ==> 2
