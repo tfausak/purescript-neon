5057b7b65be8550a12871df03310727e8cb9a4fe
Taylor Fausak <taylor@fausak.me>
2015-11-11 13:18:01 -0600

    Add tests for the rest of StateT's instances

--- test/Neon/Transformers/StateT.purs
+++ test/Neon/Transformers/StateT.purs
@@ -9,10 +9,10 @@ testStateT :: Test
 testStateT = do
   info "Neon.Transformers.StateT"
   let x = StateT (\ x -> pure (pair 0 x))
-  -- TODO: alternative
+  runStateT (StateT (constant []) <|> StateT (\ y -> pure (pair 0 y))) 2 ==> [pair 0 2]
   runStateT (StateT (\ y -> pure (pair (+ 1) y)) <*> x) 2 ==> Identity (pair 1 2)
   runStateT (x >>= ((+ 1) >> pure)) 2 ==> Identity (pair 1 2)
-  -- TODO: empty
+  runStateT empty 2 ==> Nothing :: Maybe (Pair Unit Int)
   runStateT (lift (pure 1)) 2 ==> Identity (pair 1 2)
   runStateT ((+ 1) <$> x) 2 ==> Identity (pair 1 2)
   runStateT (pure 1) 2 ==> Identity (pair 1 2)
