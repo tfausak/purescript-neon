45eb3a4ce1dae2ddf142d4454ded49bba8eff315
Taylor Fausak <taylor@fausak.me>
2015-11-14 14:40:37 -0600

    Document the effects

--- src/Neon/Effects/Console.purs
+++ src/Neon/Effects/Console.purs
@@ -1,17 +1,45 @@
 module Neon.Effects.Console
   ( CONSOLE()
+  , error
   , info
   , log
   , warn
-  , error
   ) where
 
 import Neon.Effects.Effect (Effect())
 import Neon.Values.Unit (Unit())
 
+-- | Represents effects that can write to the console.
 foreign import data CONSOLE :: !
 
+-- | Writes an info-level message to the console.
+-- |
+-- | ``` purescript
+-- | info "Starting..."
+-- | -- prints "Starting..." to STDOUT
+-- | ```
 foreign import info :: forall e. String -> Effect (console :: CONSOLE | e) Unit
+
+-- | Writes a log-level message to the console.
+-- |
+-- | ``` purescript
+-- | log "Listening.."
+-- | -- prints "Listening..." to STDOUT
+-- | ```
 foreign import log :: forall e. String -> Effect (console :: CONSOLE | e) Unit
+
+-- | Writes a warn-level message to the console.
+-- |
+-- | ``` purescript
+-- | warn "Stopping..."
+-- | -- prints "Stopping..." to STDERR
+-- | ```
 foreign import warn :: forall e. String -> Effect (console :: CONSOLE | e) Unit
+
+-- | Writes an error-level message to the console.
+-- |
+-- | ``` purescript
+-- | error "Exploded!"
+-- | -- prints "Exploded!" to STDERR
+-- | ```
 foreign import error :: forall e. String -> Effect (console :: CONSOLE | e) Unit
