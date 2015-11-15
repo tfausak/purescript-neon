8b3516c8f631f286e27f7338924826c9926ca10a
Taylor Fausak <taylor@fausak.me>
2015-11-15 15:11:00 -0600

    Document most types

--- src/Neon/Types/HasDivide.purs
+++ src/Neon/Types/HasDivide.purs
@@ -13,11 +13,26 @@ foreign import nativeDivideInt :: Int -> Int -> Int
 foreign import nativeModuloInt :: Int -> Int -> Int
 foreign import nativeDivideNumber :: Number -> Number -> Number
 
+-- | Represents types that are divisible.
+-- |
 -- | Laws:
 -- | - Remainder: `(x / y) * y + (x % y) = x`
 -- | - Identity: `x / one = x`
 class (HasOne a) <= HasDivide a where
+  -- | Divides one value by another.
+  -- |
+  -- | ``` purescript
+  -- | divide 5 2
+  -- | -- 2
+  -- | ```
   divide :: a -> a -> a
+
+  -- | Returns the remainder after division.
+  -- |
+  -- | ``` purescript
+  -- | modulo 5 2
+  -- | -- 1
+  -- | ```
   modulo :: a -> a -> a
 
 instance functionHasDivide :: (HasDivide b) => HasDivide (a -> b) where
@@ -33,14 +48,30 @@ instance numberHasDivide :: HasDivide Number where
   modulo _ _ = 0.0
 
 -- | Alias for `divide`.
+-- |
+-- | ``` purescript
+-- | 5 / 2
+-- | -- 2
+-- | ```
 (/) :: forall a. (HasDivide a) => a -> a -> a
 (/) x y = divide x y
 infixl 7 /
 
 -- | Alias for `modulo`.
+-- |
+-- | ``` purescript
+-- | 5 % 2
+-- | -- 1
+-- | ```
 (%) :: forall a. (HasDivide a) => a -> a -> a
 (%) x y = modulo x y
 infixl 7 %
 
+-- | Returns the multiplicative inverse.
+-- |
+-- | ``` purescript
+-- | reciprocal 2
+-- | -- 0.5
+-- | ```
 reciprocal :: forall a. (HasDivide a) => a -> a
 reciprocal x = one / x
--- src/Neon/Types/HasZero.purs
+++ src/Neon/Types/HasZero.purs
@@ -6,9 +6,18 @@ module Neon.Types.HasZero
 import Neon.Primitives.Function (always)
 import Neon.Types.HasAdd (HasAdd)
 
+-- | Represents types that have an additive identity. This is also known as a
+-- | [monoid](https://en.wikipedia.org/wiki/Monoid).
+-- |
 -- | Laws:
 -- | - Identity: `zero + x = x + zero = x`
 class (HasAdd a) <= HasZero a where
+  -- | Returns the additive identity value.
+  -- |
+  -- | ``` purescript
+  -- | zero
+  -- | -- 0
+  -- | ```
   zero :: a
 
 instance arrayHasZero :: HasZero (Array a) where
