cbe941c0693086afff1f4fde931374a47b1e7238
Taylor Fausak <taylor@fausak.me>
2015-11-15 19:39:52 -0600

    Document HasTraverse

--- src/Neon/Types/HasTraverse.purs
+++ src/Neon/Types/HasTraverse.purs
@@ -10,7 +10,22 @@ import Neon.Types.HasPure (HasPure, pure)
 import Neon.Values.List (List(Nil, Cons), toList, fromList)
 import Neon.Values.Maybe (Maybe(Nothing, Just))
 
+-- | Represents data structures that can be traversed from left to right.
+-- | Unlike `HasFold`, these structures can be traversed while keeping their
+-- | shape.
+-- |
+-- | Laws:
+-- | - Naturality: `t >> traverse f = traverse (t >> f)`
+-- | - Identity: `traverse Identity = Identity`
+-- | - Composition: TODO
 class (HasFold t, HasMap t) <= HasTraverse t where
+  -- | Map each element of a structure to an action, evaluate these actions
+  -- | from left to right, and collect the results.
+  -- |
+  -- | ``` purescript
+  -- | traverse (show >> Just) [1, 2]
+  -- | -- Just ["1", "2"]
+  -- | ```
   traverse :: forall a b m. (HasPure m) => (a -> m b) -> t a -> m (t b)
 
 instance arrayHasTraverse :: HasTraverse Array where
