053de13dcd586fb3d2daad653596d135cf918620
Taylor Fausak <taylor@fausak.me>
2015-11-25 09:29:31 -0600

    Replace "toList" with "fromArray"

--- test/Neon/Values/List.purs
+++ test/Neon/Values/List.purs
@@ -11,8 +11,8 @@ main = do
   Cons 1 Nil ==> Cons 1 Nil
   foldl (+) "a" (Cons "b" (Cons "c" Nil)) ==> "abc"
   foldr (+) "a" (Cons "b" (Cons "c" Nil)) ==> "bca"
+  fromArray [1, 2] ==> Cons 1 (Cons 2 Nil)
   (+ 1) <$> Cons 1 (Cons 2 Nil) ==> Cons 2 (Cons 3 Nil)
   show (Nil :: List Unit) ==> "Nil"
   show (Cons 1 Nil) ==> "Cons (1) (Nil)"
-  toList [1, 2] ==> Cons 1 (Cons 2 Nil)
   toArray (Cons 1 (Cons 2 Nil)) ==> [1, 2]
