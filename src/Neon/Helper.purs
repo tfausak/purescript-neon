103edda8ed3ca21f7e763ad02496c7c44552f597
Taylor Fausak <taylor@fausak.me>
2015-12-06 00:03:01 -0600

    Add some helpers

--- src/Neon/Helper.purs
+++ src/Neon/Helper.purs
@@ -1,20 +1,31 @@
 module Neon.Helper
-  ( greaterOrEqual
+  ( for
+  , greaterOrEqual
   , lessOrEqual
   , negate
   , notEqual
+  , product
   , reciprocal
+  , sum
   ) where
 
+import Neon.Class.Add (Add, add)
 import Neon.Class.Divide (Divide, divide)
 import Neon.Class.Equal (Equal, equal)
+import Neon.Class.Flip (flip)
 import Neon.Class.Greater (Greater, greater)
 import Neon.Class.Less (Less, less)
+import Neon.Class.Map (Map, map)
+import Neon.Class.Multiply (Multiply, multiply)
 import Neon.Class.Not (not)
 import Neon.Class.One (One, one)
 import Neon.Class.Or (or)
+import Neon.Class.Reduce (Reduce, reduce)
 import Neon.Class.Subtract (Subtract, subtract)
 import Neon.Class.Zero (Zero, zero)
+
+for :: forall a b c. (Map a) => a b -> (b -> c) -> a c
+for = flip map
 
 greaterOrEqual :: forall a. (Equal a, Greater a) => a -> a -> Boolean
 greaterOrEqual x = or (greater x) (equal x)
@@ -28,5 +39,11 @@ negate = subtract zero
 notEqual :: forall a. (Equal a) => a -> a -> Boolean
 notEqual = not equal
 
+product :: forall a b. (Multiply b, One b, Reduce a) => a b -> b
+product = reduce multiply one
+
 reciprocal :: forall a. (Divide a, One a) => a -> a
 reciprocal = divide one
+
+sum :: forall a b. (Add b, Reduce a, Zero b) => a b -> b
+sum = reduce add zero
--- test/Neon/HelperSpec.purs
+++ test/Neon/HelperSpec.purs
@@ -5,8 +5,11 @@ import Test.Spec
 
 main :: Spec
 main = describe "Neon.Helper" do
+  for [1, 2] (+ 1) `shouldBe` [2, 3]
   greaterOrEqual 2 1 `shouldBe` true
   lessOrEqual 1 2 `shouldBe` true
   negate 1 `shouldBe` -1
   notEqual false true `shouldBe` true
+  product [2, 3] `shouldBe` 6
   reciprocal 2.0 `shouldBe` 0.5
+  sum [2, 3] `shouldBe` 5
