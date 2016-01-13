2bb581c15d810ea85a5b0976f9ccbe9113dc1c46
Taylor Fausak <taylor@fausak.me>
2016-01-12 20:41:51 -0600

    Rename "error" to "exception"

--- src/Neon/Data.purs
+++ src/Neon/Data.purs
@@ -1,13 +1,15 @@
 module Neon.Data
-  ( module Control.Monad.Eff.Exception
-  , module Data.List
-  , module Data.Maybe
-  , module Data.Tuple
-  , module Prelude
+  ( module Neon.Data
+  , module X
   ) where
 
-import Control.Monad.Eff.Exception (Error(), error)
-import Data.List (List(Nil, Cons))
-import Data.Maybe (Maybe(Nothing, Just))
-import Data.Tuple (Tuple(Tuple))
-import Prelude (Unit(), unit)
+import Control.Monad.Eff.Exception (Error()) as X
+import Data.List (List(Nil, Cons)) as X
+import Data.Maybe (Maybe(Nothing, Just)) as X
+import Data.Tuple (Tuple(Tuple)) as X
+import Prelude (Unit(), unit) as X
+
+import Control.Monad.Eff.Exception as Exception
+
+exception :: String -> Exception.Error
+exception = Exception.error
