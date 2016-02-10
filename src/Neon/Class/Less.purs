9d5f4f3cb5536097ec1a9df2e717548108809edf
Taylor Fausak <taylor@fausak.me>
2016-02-09 19:40:16 -0600

    Add Less instances for Array and List

--- src/Neon/Class/Less.purs
+++ src/Neon/Class/Less.purs
@@ -1,12 +1,17 @@
 module Neon.Class.Less where
 
+import Neon.Class.Equal as Equal
+import Neon.Class.FromArray as FromArray
+import Neon.Data as Data
 import Prelude as Prelude
 
 class Less a where
   less :: a -> a -> Boolean
 
--- instance lessArray :: (Equal.Equal a, Less a) => Less (Array a) where
---   less ys xs = false -- TODO
+instance lessArray :: (Equal.Equal a, Less a) => Less (Array a) where
+  less ys xs =
+    let toList = FromArray.fromArray :: Array a -> Data.List a
+    in  less (toList ys) (toList xs)
 
 instance lessBoolean :: Less Boolean where
   less y x = x Prelude.< y
@@ -17,8 +22,13 @@ instance lessChar :: Less Char where
 instance lessInt :: Less Int where
   less y x = x Prelude.< y
 
--- instance lessList :: (Equal.Equal a, Less a) => Less (Data.List a) where
---   less ys xs = false -- TODO
+instance lessList :: (Equal.Equal a, Less a) => Less (Data.List a) where
+  less ys xs = case { xs: xs, ys: ys } of
+    { xs: Data.Nil, ys: Data.Cons _ _ } -> true
+    { xs: Data.Cons xh xt, ys: Data.Cons yh yt } -> if Equal.equal yh xh
+      then less yt xt
+      else less yh xh
+    _ -> false
 
 instance lessNumber :: Less Number where
   less y x = x Prelude.< y
--- test/Neon/Class/LessTest.purs
+++ test/Neon/Class/LessTest.purs
@@ -6,6 +6,9 @@ import Test.Helper (Suite, bind, quickCheck, test, (===))
 
 suite :: Suite
 suite = test "Less" do
+  test "Array" do
+    quickCheck \ (x :: Array Int) y ->
+      Neon.less y x === x Prelude.< y
   test "Boolean" do
     quickCheck \ (x :: Boolean) y ->
       Neon.less y x === x Prelude.< y
@@ -14,6 +17,9 @@ suite = test "Less" do
       Neon.less y x === x Prelude.< y
   test "Int" do
     quickCheck \ (x :: Int) y ->
+      Neon.less y x === x Prelude.< y
+  test "List" do
+    quickCheck \ (x :: Neon.List Int) y ->
       Neon.less y x === x Prelude.< y
   test "Number" do
     quickCheck \ (x :: Number) y ->
