d295bafa4b2ade139f1b6a843e55674d571050c6
Taylor Fausak <taylor@fausak.me>
2016-04-30 09:27:41 -0500

    Document the top-level module

--- src/Neon.purs
+++ src/Neon.purs
@@ -1,3 +1,33 @@
+-- | This is the top-level entry point into Neon. Typically to use Neon you
+-- | would simply import this unqualified.
+-- |
+-- | ``` purescript
+-- | import Neon
+-- | ```
+-- |
+-- | If you want to avoid namespace collisions, you can import Neon qualified.
+-- | It is a good idea to still import the operators unqualified, otherwise it
+-- | will be annoying to use.
+-- |
+-- | ``` purescript
+-- | import Neon as N
+-- | import Neon.Operator
+-- | ```
+-- |
+-- | Here is an overview of the re-exported modules:
+-- |
+-- | - Primitive: Functions for stuff built into PureScript, like `Number`.
+-- |   This is for basics like converting between numeric types.
+-- | - Data: Re-exports from other libraries. Neon does not define any of its
+-- |   own data types.
+-- | - Effect: More re-exports. Neon also does not define any of its own effect
+-- |   types.
+-- | - Class: This is why Neon exists. These classes are what separate Neon
+-- |   from the `Prelude`.
+-- | - Helper: Things built on top of everything else. These are all nice to
+-- |   have but can be derived from simpler things.
+-- | - Operator: A handful of operators for doing common tasks. Neon doesn't go
+-- |   overboard with operators like many other PureScript libraries.
 module Neon
   ( module Neon.Class
   , module Neon.Data
